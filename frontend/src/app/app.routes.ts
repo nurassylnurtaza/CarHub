3bd40028ca42206d30e0b38bd486ecd39b16c9eb
balgabekj <b_zhaksylyk@kbtu.kz>
2024-04-21 18:26:03 +0600

    baga upd

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes, RouterModule  } from '@angular/router';
 import {NgModule} from "@angular/core";
 import {HomeComponent} from "./components/home/home.component";
+import {NotFoundComponent} from "./components/not-found/not-found.component";
 export const routes: Routes = [
-  {path:'', component: HomeComponent},
-  {path:'', component: HomeComponent},
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotFoundComponent }
 ];
