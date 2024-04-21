f95fffff26035758d341a1b40ea60f24b70c6ab0
balgabekj <b_zhaksylyk@kbtu.kz>
2024-04-21 16:47:35 +0600

    baga upd

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Routes, RouterModule  } from '@angular/router';
+import {NgModule} from "@angular/core";
+import {HomeComponent} from "./components/home/home.component";
+export const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'', component: HomeComponent},
+];
---
+++ frontend/src/app/components/car-list/car-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './car-list.component.html',
+  styleUrl: './car-list.component.css'
+})
+export class CarListComponent {
+
+}
